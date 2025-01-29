09574cf9d24fede8694931dabf51d829aca32074
Shreya Parida <shreya.parida.2004@gmail.com>
2025-01-29 11:21:47 +0530

    Add files via upload

---
+++ MINIONcodes.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+print("THE MINI CODES OF PYTHON!")
+
+
+# # Sum of Two Numbers
+
+# In[19]:
+
+
+a = 5990
+b = 1090
+print(a + b)
+
+
+# In[20]:
+
+
+a = 65723
+b = 896764
+print(a - b)
+
+
+# # Check Even or Odd
+
+# In[17]:
+
+
+num = 7268638
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# In[18]:
+
+
+num = 98633567
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# # Find Maximum of Two Numbers
+
+# In[16]:
+
+
+x = 12684
+y = 8647832
+print(max(x, y))
+
+
+# In[21]:
+
+
+u = 98765
+v = 54321
+print(max(u, v))
+
+
+# # Simple Countdown (10 to 1)
+
+# In[5]:
+
+
+for i in range(10, 0, -1):
+    print(i)
+
+
+# In[23]:
+
+
+for github in range(15, 0, -1):
+    print(github)
+
+
+# # Simple Multiplication Table
+
+# In[13]:
+
+
+num = 25
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+
+# In[24]:
+
+
+num = 525
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+
+# # Simple Square of a Number
+
+# In[14]:
+
+
+num = 365
+print(num ** 2)
+
+
+# In[25]:
+
+
+num = 366
+print(num ** 2)
+
+
+# # Print Characters in a String
+
+# In[15]:
+
+
+text = "Github"
+for char in text:
+    print(char)
+
+
+# In[26]:
+
+
+text = "ShreyaParida"
+for char in text:
+    print(char)
+

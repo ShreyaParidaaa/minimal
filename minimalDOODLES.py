d114e0ffd5e5a49bc32ac5280c21aaf639215607
Shreya Parida <shreya.parida.2004@gmail.com>
2025-01-30 11:24:14 +0530

    Add files via upload

---
+++ minimalDOODLES.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# meow!
+
+# Bunny🐰
+
+# In[5]:
+
+
+print(" (\(\  ")
+print(" ( -.-) ") 
+print(" o_(\")(\") ")
+
+
+# Dog🐶
+
+# In[6]:
+
+
+print(" / \\__  ")
+print("(    @\\____ ")
+print(" /         O ")
+print("/   (_____  ")
+print("\\_____/  U  ")
+
+
+# Sleeping Cat😺
+
+# In[7]:
+
+
+print(" /\_/\  zZ ")
+print("( -.- ) ")
+print(" >  💤  < ")
+
+
+# Little Duck🦆
+
+# In[8]:
+
+
+print("  _  ")
+print(" (o>)  ")
+print(" //\\  ")
+print(" V_/_  ")
+
+
+# Cute Bear🐻
+
+# In[9]:
+
+
+print(" ʕ•ᴥ•ʔ ")
+
+
+# Little Fish 🐟
+
+# In[3]:
+
+
+print(" ><(((º>  ")
+
